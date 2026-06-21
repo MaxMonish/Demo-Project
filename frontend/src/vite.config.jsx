271c89a65320c0c974e1bac9224f2c91bcad432c
Monish <monishbaskar01234@gmail.com>
2026-06-21 20:00:54 +0530

    Fix import path case for PageLoader component

--- frontend/src/vite.config.jsx
+++ frontend/src/vite.config.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
-import PageLoader from "../../components/Common/PageLoader";
+import PageLoader from "../../components/common/PageLoader";
 
 export default defineConfig({
   plugins: [react()],
